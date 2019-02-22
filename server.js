a18c9ad74201c710d8661f3e2287933d38d0d5ef
Ethan Gent <ethan.gent.2391@gmail.com>
2019-02-22 01:32:42 -0500

    declared DEPENDECIES AND PORT

---
+++ server.js
@@ -0,0 +1,6 @@
+var EXPRESS = require("express");
+var BP = require("body-parser");
+var METH_OR = require("method-override");
+var HANDLEBARS = require("express-handlebars");
+var APP = EXPRESS();
+var PORT = process.env.PORT || 3000;
